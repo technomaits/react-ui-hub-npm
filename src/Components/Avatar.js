f6bd20898937d8fe3829c612023448d6ae5b90c4
sahil <sahilaroraji2002@gmail.com>
2022-10-18 17:32:39 +0530

    first commit

---
+++ src/Components/Avatar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Avatar =(props)=>{
+
+return(
+
+    <img
+    className="avatar"
+    style={{ width: "50px", height: "50px" , borderRadius:"50px",border:"0.5px solid black" }}
+    src="https://www.w3schools.com/howto/img_avatar2.png"
+    alt=""
+  />
+
+);
+
+}
+
+export default Avatar;
+
---
+++ src/Components/Chip.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Chip = (props) => {
+
+    return (
+
+        <p className="chip" style={{ backgroundColor: "#ccc4c4", width: "fit-content", padding: "7px", paddingLeft: "15px", paddingRight: "15px", textAlign: "center", height: "20px", borderRadius: "20px", marginTop: "1px", marginBottom: "1px", paddingBottom: "10px" }}>
+           success
+        </p>
+
+    );
+
+
+}
+
+export default Chip;
